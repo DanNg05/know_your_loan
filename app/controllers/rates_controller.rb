912ae1f80fc4aa45d4a83810746813ad81aaaf4b
Doris Xu <dorisxjt@gmail.com>
2024-04-23 20:43:15 +1000

    update on rate table

--- app/controllers/rates_controller.rb
+++ app/controllers/rates_controller.rb
@@ -1,28 +1,25 @@
 require 'open-uri'
 class RatesController < ApplicationController
-  def rate
-    @rates = Array.new
-    html_content = URI.open('https://www.mortgagerates.co.nz/mortgage-rates').read
-    doc = Nokogiri::HTML.parse(html_content)
-    doc.search('.rates-table-wrapper .table-row').each_with_index do |element, index|
-      # puts "#{index + 1}. #{element.text.strip}"
-      table_cells=element.search('.table-cell')
-      image=element.search('.bank-logo-container img')
-      bank_html = element.search('.product-name')
-      bank_name = bank_html.text.strip
-      first_year = table_cells[1].text.strip
-      second_year = table_cells[2].text.strip
-      third_year = table_cells[3].text.strip
-      @rates << {
-        bank_name: bank_name,
-        first_year: first_year,
-        third_year: third_year,
-        second_year: second_year,
-        image: image.attr('src').to_s,
-      }
-    end
-  end
+ def rate
+   @rates = Array.new
+   html_content = URI.open('https://www.mortgagerates.co.nz/mortgage-rates').read
+   doc = Nokogiri::HTML.parse(html_content)
+   doc.search('.rates-table-wrapper .table-row').each_with_index do |element, index|
+     # puts "#{index + 1}. #{element.text.strip}"
+     table_cells=element.search('.table-cell')
+     image=element.search('.bank-logo-container img')
+     bank_html = element.search('.product-name')
+     bank_name = bank_html.text.strip
+     one_year = table_cells[1].text.strip
+     two_years = table_cells[2].text.strip
+     three_years = table_cells[3].text.strip
+     @rates << {
+       bank_name: bank_name,
+       one_year: one_year,
+       three_years: three_years,
+       two_years: two_years,
+       image: image.attr('src').to_s,
+     }
+   end
+ end
 end
-# narrow down table_cells[0]
-#add image instead of name possibly
-#models and db, workers(sidekiq) reun every 12/24 hours, retrieve data from database
