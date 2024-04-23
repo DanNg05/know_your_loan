99bb56f1cf6353a09dfe885a3d6ab09dbefc84d3
Dan Nguyen <sydan05071997@gmail.com>
2024-04-23 20:52:53 +0930

    Boostrap modal for appointment, date picker for appointment, GEOMAP for brokers

--- app/models/broker.rb
+++ app/models/broker.rb
@@ -1,4 +1,6 @@
 class Broker < ApplicationRecord
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 
   has_many :reviews, dependent: :destroy
   has_many :appointments, dependent: :destroy
