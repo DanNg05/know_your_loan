cdd7f8dd7c85b12c0b6ceba6b8f87fb320cb23b8
Dan Nguyen <sydan05071997@gmail.com>
2024-04-28 20:46:30 +0930

    cannot install chart.js

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    @lowest_rate = @rates[:one_year].order.first
+    # @lowest_rate = @rates[:one_year].order.first
+  end
+
+  def dashboard
+    @mortgage = Mortgage.last
   end
   end
