3b0aae24b508d1f4ac4720cda186cdd8f4550023
Dan Nguyen <sydan05071997@gmail.com>
2024-05-07 20:57:41 +0930

    need to fix JS for review form

--- app/javascript/controllers/review_toggle_controller.js
+++ app/javascript/controllers/review_toggle_controller.js
@@ -1,13 +1,38 @@
 import { Controller } from "@hotwired/stimulus"
+import Swal from "sweetalert2"
 
 // Connects to data-controller="review-toggle"
 export default class extends Controller {
-  static targets = ["reviewForm"]
+  static targets = ["reviewForm", "content", "rating", "reviewSubmit"]
   connect() {
     // console.log("Hello there")
   }
 
   fire() {
     this.reviewFormTarget.classList.toggle("d-none")
+  }
+
+  initSweetalert(event) {
+    event.preventDefault();
+    if (this.contentTarget.value.length === 0 || this.ratingTarget.value.length === 0) {
+      Swal.fire({
+        title: "Missing Input",
+        icon: "question",
+        text: "Please fill the inputs!"
+      })
+    }
+    else {
+      Swal.fire({
+        title: "Your Review",
+        icon: "success",
+        text: "Your review is successfully created!"
+      }).then((result) => {
+        /* Read more about isConfirmed, isDenied below */
+        if (result.isConfirmed) {
+          location.reload();
+        }
+        this.reviewSubmitTarget.submit()
+      })
+    }
   }
 }
